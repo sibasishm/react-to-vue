8fa96786cce5ac617247ba0e15822d07ce24767a
Sibasish <smsibasish@gmail.com>
2025-05-13 17:09:19 +0530

    setup supabase and picocss

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react';
+import supabase from './utils/supabase-client';
 
 function App() {
-  const [count, setCount] = useState(0)
+	const [todos, setTodos] = useState<any[]>([]);
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	useEffect(() => {
+		async function getTodos() {
+			const { data: todos } = await supabase.from('todos').select();
+
+			if (todos && todos.length > 1) {
+				setTodos(todos);
+			}
+		}
+
+		getTodos();
+	}, []);
+
+	return (
+		<div>
+			<h1>Todo List</h1>
+			<ol>
+				{todos.map(todo => (
+					<li key={todo}>{todo}</li>
+				))}
+			</ol>
+		</div>
+	);
 }
-
-export default App
+export default App;
